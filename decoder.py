8e989828c04d0f892070a4ad0362aa7ad4f69602
Zac Pullar-Strecker <zacmps@gmail.com>
2019-10-19 19:53:44 +1300

    Initial commit

---
+++ decoder.py
@@ -0,0 +1,58 @@
+import sys
+import gzip
+
+if len(sys.argv) != 3:
+    print("Expected two arguments, in file and out file")
+    sys.exit(1)
+FILE = sys.argv[1]
+OUT = sys.argv[2]
+
+# Taken from https://en.wikipedia.org/wiki/Whitespace_character#Unicode
+ALPHABET = "\u200B\u200C\u200D\u2060\uFEFF"
+NULL = "\u180E"
+
+# From https://github.com/yamatt/python-dencoder
+class Dencoder():
+    def __init__(self, alphabet):
+        """
+        Set up the decoder/encoder alphabet.
+        """
+        self.alphabet = alphabet
+        self.base = len(self.alphabet)
+
+    def encode(self, number):
+        """
+        Turns a number in to a string that represents it.
+        """
+        if number == 0:
+            return self.alphabet[0]
+        s = ''
+        while number > 0:
+            s += self.alphabet[number % self.base]
+            number = number // self.base
+
+        return s[::-1] # reverse string
+
+    def decode(self, s):
+        """
+        Turns string encoding back in to a number.
+        """
+        i = 0
+        for char in s:
+            i = i * self.base + self.alphabet.index(char)
+        return i
+
+decoder = Dencoder(ALPHABET)
+
+print('Decoding')
+new = open(FILE, 'r', encoding='utf8')
+encoded = new.read().split(NULL)
+decoded = bytearray()
+for i, s in enumerate(encoded):
+    d = decoder.decode(s)
+    decoded.append(d)
+
+print('Decompressing')
+decompressed = gzip.decompress(decoded)
+with open(OUT, 'wb') as decompressed_f:
+    decompressed_f.write(decompressed)
---
+++ encoder.py
@@ -0,0 +1,66 @@
+import gzip
+import os
+import sys
+
+if len(sys.argv) != 3:
+    print("Expected two arguments, in file and out file")
+    sys.exit(1)
+FILE = sys.argv[1]
+OUT = sys.argv[2]
+
+print('Opening file')
+print(f'File length: {os.stat(FILE).st_size} bytes')
+with open(FILE, encoding='utf8') as file:
+
+    print('Compressing text')
+    compressed = gzip.compress(bytes(file.read(), "utf8"))
+
+# Taken from https://en.wikipedia.org/wiki/Whitespace_character#Unicode
+ALPHABET = "\u200B\u200C\u200D\u2060\uFEFF"
+NULL = "\u180E"
+
+# From https://github.com/yamatt/python-dencoder
+class Dencoder():
+    def __init__(self, alphabet):
+        """
+        Set up the decoder/encoder alphabet.
+        """
+        self.alphabet = alphabet
+        self.base = len(self.alphabet)
+
+    def encode(self, number):
+        """
+        Turns a number in to a string that represents it.
+        """
+        if number == 0:
+            return self.alphabet[0]
+        s = ''
+        while number > 0:
+            s += self.alphabet[number % self.base]
+            number = number // self.base
+
+        return s[::-1] # reverse string
+
+    def decode(self, s):
+        """
+        Turns string encoding back in to a number.
+        """
+        i = 0
+        for char in s:
+            i = i * self.base + self.alphabet.index(char)
+        return i
+
+print('Encoding')
+encoder = Dencoder(ALPHABET)
+encoded = []
+for byte in compressed:
+    encoded.append(encoder.encode(byte))
+
+print('Consistence check')
+for i, byte in enumerate(encoded):
+    assert encoder.decode(byte) == compressed[i]
+
+print('Writing encoded file to out.txt')
+with open('out.txt', 'w', encoding='utf8') as out:
+    out.write(NULL.join(encoded))
+print(f'File length: {os.stat("out.txt").st_size} bytes')
